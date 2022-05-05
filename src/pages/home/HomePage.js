c263aeea9df1e919e43c082666766ee0d32989e4
Goga <gogaimerlishvili1@gmail.com>
2022-05-06 00:57:40 +0400

    navbar content

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,7 +1,20 @@
 import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./home.css";
 
 const HomePage = () => {
-  return <Topbar />;
+  return (
+    <>
+      <Topbar />
+      <div className="home__Container">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
