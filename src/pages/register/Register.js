2836c21514fd0b2aa2f8d2d6b8428c7c5ced8b07
Goga <gogaimerlishvili1@gmail.com>
2022-05-07 02:08:22 +0400

    Added pages

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,6 +1,6 @@
-import "./login.css";
+import "./register.css";
 
-export default function Login() {
+export default function Register() {
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -12,13 +12,12 @@ export default function Login() {
         </div>
         <div className="loginRight">
           <div className="loginBox">
+            <input placeholder="Username" className="loginInput" />
             <input placeholder="Email" className="loginInput" />
             <input placeholder="Password" className="loginInput" />
-            <button className="loginButton">Log In</button>
-            <span className="loginForgot">Forgot Password?</span>
-            <button className="loginRegisterButton">
-              Create a New Account
-            </button>
+            <input placeholder="Password Again" className="loginInput" />
+            <button className="loginButton">Sign Up</button>
+            <button className="loginRegisterButton">Log into Account</button>
           </div>
         </div>
       </div>
